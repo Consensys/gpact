549c98630470fe5889c697d4ab0aa2c5a8cb5f67
Peter  Robinson <drinkcoffee@eml.cc>
2021-01-29 10:38:08 +1000

    Change Copyright from ConsenSys AG to ConsenSys Software
    Signed-off-by: Peter  Robinson <drinkcoffee@eml.cc>

--- registrar/src/main/solidity/VotingAlgInterface.sol
+++ registrar/src/main/solidity/VotingAlgInterface.sol
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 ConsenSys AG.
+ * Copyright 2018 ConsenSys Software Inc
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
