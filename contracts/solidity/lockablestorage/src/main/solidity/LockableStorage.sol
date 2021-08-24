714b0360bc72f3a922a61643dd0a7456f8b46942
Peter Robinson <drinkcoffee@eml.cc>
2021-08-24 16:59:18 +1000

    Determine is locked indirectly, thus reduce the number of storage locations and gas used
    Signed-off-by: Peter Robinson <drinkcoffee@eml.cc>

--- contracts/solidity/lockablestorage/src/main/solidity/LockableStorage.sol
+++ contracts/solidity/lockablestorage/src/main/solidity/LockableStorage.sol
@@ -32,14 +32,14 @@ abstract contract LockableStorage {
     mapping(uint256=>uint256) private dataStore;
     // Provisional updates for data storage keys to values stored.
     mapping(uint256=>uint256) private provisionalUpdates;
-    // Data storage keys to bool indicating if values stored.
-    mapping(uint256=>bool) public isLocked;
 
     // Map of list of keys for values that have been updated / are locked.
     // Map key is keccak256(root bc id, transaction id)
     mapping(bytes32=>uint256[]) private provisionalUpdatesLists;
 
     // Map of data storage key to blockchain / transaction id that locked this item.
+    // If the mapping shows that the blockchain / transaction id is zero, this indicates
+    // that the location is not locked.
     mapping(uint256=>bytes32) public lockedByRootBlockchainIdTransactionId;
 
 
@@ -56,7 +56,7 @@ abstract contract LockableStorage {
      */
     function setUint256(uint256 _key, uint256 _val) internal {
         //   If item locked {throw an error}
-        if (isLocked[_key]) {
+        if (isLocked(_key)) {
             revert("Contract item locked");
         }
 
@@ -71,7 +71,6 @@ abstract contract LockableStorage {
             crossBlockchainControl.addToListOfLockedContracts(address(this));
             provisionalUpdatesLists[index].push(_key);
             provisionalUpdates[_key] = _val;
-            isLocked[_key] = true;
         }
     }
 
@@ -139,8 +138,8 @@ abstract contract LockableStorage {
             }
             // Delete provisional updates.
             delete provisionalUpdates[key];
-            // Unlock
-            delete isLocked[key];
+            // Delete locked by indicator.
+            delete lockedByRootBlockchainIdTransactionId[key];
         }
         // Delete the list of updates.
         delete provisionalUpdatesLists[index];
@@ -155,7 +154,7 @@ abstract contract LockableStorage {
 
 
     function getUint256(uint256 _key) internal view returns(uint256) {
-        if (isLocked[_key]) {
+        if (isLocked(_key)) {
             revert("Read while contract item locked");
         }
         return dataStore[_key];
@@ -164,7 +163,7 @@ abstract contract LockableStorage {
         return dataStore[_key];
     }
     function getUint256Provisional(uint256 _key) internal view returns(uint256) {
-        if (isLocked[_key]) {
+        if (isLocked(_key)) {
             return provisionalUpdates[_key];
         }
         return dataStore[_key];
@@ -205,7 +204,7 @@ abstract contract LockableStorage {
     }
     function isMapValueLocked(uint256 _key, uint256 _mapKey) internal view returns(bool) {
         bytes32 index = keccak256(abi.encodePacked(_key, _mapKey));
-        return isLocked[uint256(index)];
+        return isLocked(uint256(index));
     }
 
     function getDoubleMapValue(uint256 _key, uint256 _mapKey1, uint256 _mapKey2) internal view returns(uint256) {
@@ -222,7 +221,7 @@ abstract contract LockableStorage {
     }
     function isDoubleMapValueLocked(uint256 _key, uint256 _mapKey1, uint256 _mapKey2) internal view returns(bool) {
         bytes32 index = keccak256(abi.encodePacked(_key, _mapKey1, _mapKey2));
-        return isLocked[uint256(index)];
+        return isLocked(uint256(index));
     }
 
     function getTripleMapValue(uint256 _key, uint256 _mapKey1, uint256 _mapKey2, uint256 _mapKey3) internal view returns (uint256) {
@@ -239,7 +238,11 @@ abstract contract LockableStorage {
     }
     function isTripleMapValueLocked(uint256 _key, uint256 _mapKey1, uint256 _mapKey2, uint256 _mapKey3) internal view returns (bool) {
         bytes32 index = keccak256(abi.encodePacked(_key, _mapKey1, _mapKey2, _mapKey3));
-        return isLocked[uint256(index)];
+        return isLocked(uint256(index));
+    }
+
+    function isLocked(uint256 _key) public view returns (bool) {
+        return lockedByRootBlockchainIdTransactionId[_key] != 0;
     }
 
     function provisionalUpdatesListIndex(uint256 _rootBcId, uint256 _crossTxId) private pure returns (bytes32) {
