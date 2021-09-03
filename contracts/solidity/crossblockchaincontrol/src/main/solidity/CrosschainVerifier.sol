d7c894ce1ddbc15a79245939dfe8ee67d6a7a452
Peter Robinson <drinkcoffee@eml.cc>
2021-09-03 10:16:49 +1000

    Improve documentation for verification API
    Signed-off-by: Peter Robinson <drinkcoffee@eml.cc>

--- contracts/solidity/crossblockchaincontrol/src/main/solidity/CrosschainVerifier.sol
+++ contracts/solidity/crossblockchaincontrol/src/main/solidity/CrosschainVerifier.sol
@@ -16,7 +16,22 @@ pragma solidity >=0.8;
 
 interface CrosschainVerifier {
 
-    function decodeAndVerifyEvent(uint256 _expectedBlockchainId, bytes32 _eventSig,
+    /**
+     * Decode and verify event information. Use require to fail the transaction
+     * if any of the information is invalid.
+     *
+     * @param _blockchainId The blockchain that emitted the event. This could be
+     *    used to determine which sets of signing keys are valid.
+     * @param _eventSig The event function selector. This will be for a Start event,
+     *    a Segment event, or a Root event. Not all implementations will need to
+     *    use this value. Others may need this to allow then to find the event in a
+     *    transaction receipt.
+     * @param _signedEventInfo The abi.encodePacked of the blockchain id, the Crosschain
+     *    Control contract's address, the event function selector, and the event data.
+     * @param _signature Signatures or proof information that an implementation can
+     *    use to check that _signedEventInfo is valid.
+     */
+    function decodeAndVerifyEvent(uint256 _blockchainId, bytes32 _eventSig,
         bytes calldata _signedEventInfo, bytes calldata _signature)
          external view;
 }
