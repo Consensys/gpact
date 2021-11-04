a9ae0f00abcc61697cf1e2eddd8679a85f2a59d5
Peter Robinson <drinkcoffee@users.noreply.github.com>
2021-11-05 09:00:58 +1000

    Web3j version change (#40)
    Signed-off-by: Peter Robinson <drinkcoffee@eml.cc>

--- performance/base/singlebc-write/src/main/java/net/consensys/gpact/examples/singlebc/write/soliditywrappers/ContractB.java
+++ performance/base/singlebc-write/src/main/java/net/consensys/gpact/examples/singlebc/write/soliditywrappers/ContractB.java
@@ -32,7 +32,7 @@ import org.web3j.tx.gas.ContractGasProvider;
  * or the org.web3j.codegen.SolidityFunctionWrapperGenerator in the 
  * <a href="https://github.com/web3j/web3j/tree/master/codegen">codegen module</a> to update.
  *
- * <p>Generated with web3j version 4.8.5-SNAPSHOT.
+ * <p>Generated with web3j version 4.8.7-SNAPSHOT.
  */
 @SuppressWarnings("rawtypes")
 public class ContractB extends Contract {
