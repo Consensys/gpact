115545ca6d465547091c954ac27218826dbe03b1
wcgcyx <wcgcyx@gmail.com>
2021-11-11 12:38:50 +1000

    Move getRevertMsg to common

--- functioncall/gpact/src/main/solidity/CrosschainControl.sol
+++ functioncall/gpact/src/main/solidity/CrosschainControl.sol
@@ -21,13 +21,15 @@ import "../../../../interface/src/main/solidity/LockableStorageInterface.sol";
 import "../../../../interface/src/main/solidity/CrosschainLockingInterface.sol";
 import "../../../../interface/src/main/solidity/CrosschainFunctionCallReturnInterface.sol";
 import "../../../../interface/src/main/solidity/HiddenParameters.sol";
+import "../../../../../common/common/src/main/solidity/ResponseProcessUtil.sol";
 
 contract CrosschainControl is
     CrosschainFunctionCallReturnInterface,
     CbcDecVer,
     CallPathCallExecutionTree,
     CrosschainLockingInterface,
-    HiddenParameters
+    HiddenParameters,
+    ResponseProcessUtil
 {
     event Start(
         uint256 _crossBlockchainTransactionId,
@@ -809,77 +811,5 @@ contract CrosschainControl is
         returns (bytes32)
     {
         return keccak256(abi.encodePacked(_rootBcId, _crossTxId));
-    }
-
-    //    function getRevertMsg(bytes memory _returnData) internal pure returns (string memory) {
-    //        // If the _res length is less than 68, then the transaction failed silently (without a revert message)
-    //        if (_returnData.length < 68) return 'Transaction reverted silently';
-    //
-    //        assembly {
-    //        // Slice the sighash.
-    //            _returnData := add(_returnData, 0x04)
-    //        }
-    //        return abi.decode(_returnData, (string)); // All that remains is the revert string
-    //    }
-
-    function getRevertMsg(bytes memory _returnData)
-        internal
-        pure
-        returns (string memory)
-    {
-        // A string will be 4 bytes for the function selector + 32 bytes for string length +
-        // 32 bytes for first part of string. Hence, if the length is less than 68, then
-        // this is a panic.
-        // Another way of doing this would be to look for the function selectors for revert:
-        // "0x08c379a0" = keccak256("Error(string)"
-        // or panic:
-        // "0x4e487b71" = keccak256("Panic(uint256)"
-        if (_returnData.length < 36) {
-            return
-                string(
-                    abi.encodePacked(
-                        "Revert for unknown error. Error length: ",
-                        uint2str(_returnData.length)
-                    )
-                );
-        }
-        bool isPanic = _returnData.length < 68;
-
-        assembly {
-            // Remove the function selector / sighash.
-            _returnData := add(_returnData, 0x04)
-        }
-        if (isPanic) {
-            uint256 errorCode = abi.decode(_returnData, (uint256));
-            return string(abi.encodePacked("Panic: ", uint2str(errorCode)));
-        }
-        return abi.decode(_returnData, (string)); // All that remains is the revert string
-    }
-
-    // TODO Move this to a utility sol file.
-    function uint2str(uint256 _i)
-        internal
-        pure
-        returns (string memory _uintAsString)
-    {
-        if (_i == 0) {
-            return "0";
-        }
-        uint256 j = _i;
-        uint256 len;
-        while (j != 0) {
-            len++;
-            j /= 10;
-        }
-        bytes memory bstr = new bytes(len);
-        uint256 k = len;
-        while (_i != 0) {
-            k = k - 1;
-            uint8 temp = (48 + uint8(_i - (_i / 10) * 10));
-            bytes1 b1 = bytes1(temp);
-            bstr[k] = b1;
-            _i /= 10;
-        }
-        return string(bstr);
     }
 }
