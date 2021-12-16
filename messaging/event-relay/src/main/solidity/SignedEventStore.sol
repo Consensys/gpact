5089a5f2579e79354d008a1c30e89292ba8adffe
Peter Robinson <drinkcoffee@eml.cc>
2021-12-16 11:29:32 +1000

    Fixed solidity formatting issue
    Signed-off-by: Peter Robinson <drinkcoffee@eml.cc>

--- messaging/event-relay/src/main/solidity/SignedEventStore.sol
+++ messaging/event-relay/src/main/solidity/SignedEventStore.sol
@@ -25,18 +25,16 @@ contract SignedEventStore is CrosschainVerifier, BytesUtil {
 
     struct SignedEvents {
         // The event to be actioned.
-        bytes  encodedEvent;
+        bytes encodedEvent;
         // Replay protection is the responsibility of the function call layer.
         // This variable ensures the signers who submit the event are recorded, even
         // if it is after the event is actioned.
         bool actioned;
         // List of which signers voted.
         address[] whoVoted;
-        mapping (address => bool) whoVotedMap;
+        mapping(address => bool) whoVotedMap;
     }
     mapping(bytes32 => SignedEvents) private signedEvents;
-
-
 
     constructor(address _registrar, address _functionCall) {
         registrar = AttestorSignRegistrar(_registrar);
@@ -57,11 +55,16 @@ contract SignedEventStore is CrosschainVerifier, BytesUtil {
         bytes calldata /* _signature */
     ) external view override {
         bytes32 signedEventDigest = keccak256(_encodedEvent);
-        require(signedEvents[signedEventDigest].actioned == false, "Already actioned");
+        require(
+            signedEvents[signedEventDigest].actioned == false,
+            "Already actioned"
+        );
         uint256 threshold = registrar.getSigningThreshold(_blockchainId);
-        require(signedEvents[signedEventDigest].whoVoted.length >= threshold, "Not enough signers");
+        require(
+            signedEvents[signedEventDigest].whoVoted.length >= threshold,
+            "Not enough signers"
+        );
     }
-
 
     /**
      * Relay event from one or more relayers.
@@ -74,7 +77,6 @@ contract SignedEventStore is CrosschainVerifier, BytesUtil {
         bytes calldata _encodedEvent,
         bytes calldata _signature
     ) external {
-
         // Step 1: Check the signatures of signers.
         address[] memory signers;
         bytes32[] memory sigRs;
@@ -118,7 +120,11 @@ contract SignedEventStore is CrosschainVerifier, BytesUtil {
         bytes32 signedEventDigest = keccak256(_encodedEvent);
 
         for (uint256 i = 0; i < len; i++) {
-            require(signedEvents[signedEventDigest].whoVotedMap[signers[i]] == false, "Relayer has already signed event");
+            require(
+                signedEvents[signedEventDigest].whoVotedMap[signers[i]] ==
+                    false,
+                "Relayer has already signed event"
+            );
             signedEvents[signedEventDigest].whoVotedMap[signers[i]] = true;
             signedEvents[signedEventDigest].whoVoted.push(signers[i]);
         }
@@ -132,7 +138,12 @@ contract SignedEventStore is CrosschainVerifier, BytesUtil {
         uint256 threshold = registrar.getSigningThreshold(_blockchainId);
         if (signedEvents[signedEventDigest].whoVoted.length >= threshold) {
             // NOTE: In the call below, the _signature parameter isn't used. Could be cheaper to pass bytes("")
-            functionCall.crossCallHandler(_blockchainId, _cbcAddress, _encodedEvent, _signature);
+            functionCall.crossCallHandler(
+                _blockchainId,
+                _cbcAddress,
+                _encodedEvent,
+                _signature
+            );
             signedEvents[signedEventDigest].actioned = true;
         }
     }
