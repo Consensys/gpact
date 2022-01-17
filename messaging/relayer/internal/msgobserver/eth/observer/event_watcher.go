0d6c68cd95c0012ad8f66c13858140fc6f795f4e
ermyas <ermyas.abebe@consensys.net>
2022-01-17 13:48:08 +1100

    Add finalised event watcher (#84)
    * add ability to observe finalised events
    
    * test reorg scenarios
    
    * create deterministic id scheme for events
    
    * minor refactoring
    
    * fix comment typo

--- messaging/relayer/internal/msgobserver/eth/observer/event_watcher.go
+++ messaging/relayer/internal/msgobserver/eth/observer/event_watcher.go
@@ -17,12 +17,14 @@ package observer
 
 import (
 	"context"
-	"log"
-
+	"fmt"
 	"github.com/consensys/gpact/messaging/relayer/internal/contracts/functioncall"
 	"github.com/consensys/gpact/messaging/relayer/internal/logging"
+	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/event"
+	"log"
 )
 
 // EventWatcher listens to blockchain events
@@ -31,24 +33,25 @@ type EventWatcher interface {
 }
 
 type EventWatcherConfig struct {
-	Start   *uint64
+	Start   uint64
 	Context context.Context
 	Handler EventHandler
 }
 
-// SFCCrossCallWatcher subscribes and listens to events from a simple-function-call bridge contract
-type SFCCrossCallWatcher struct {
+// SFCCrossCallRealtimeEventWatcher subscribes and listens to events from a Simple Function Call bridge contract.
+// The events produced by this watcher are generated the instant they are mined (i.e. 1 confirmation).
+// Note: The watcher does not check to see if the event is affected by any reorgs.
+type SFCCrossCallRealtimeEventWatcher struct {
 	EventWatcherConfig
 	SfcContract *functioncall.Sfc
-
-	end chan bool
+	end         chan bool
 }
 
-// Watch subscribes and starts listening to 'CrossCall' events from a given simple-function-call contract.
+// Watch subscribes and starts listening to 'CrossCall' events from a given Simple Function Call contract.
 // Events received are passed to an event handler for processing.
 // The method fails if subscribing to the event with the underlying network is not successful.
-func (l *SFCCrossCallWatcher) Watch() {
-	opts := bind.WatchOpts{Start: l.Start, Context: l.Context}
+func (l *SFCCrossCallRealtimeEventWatcher) Watch() {
+	opts := bind.WatchOpts{Start: &l.Start, Context: l.Context}
 	chanEvents := make(chan *functioncall.SfcCrossCall)
 	sub, err := l.SfcContract.WatchCrossCall(&opts, chanEvents)
 	if err != nil {
@@ -57,15 +60,15 @@ func (l *SFCCrossCallWatcher) Watch() {
 	l.start(sub, chanEvents)
 }
 
-func (l *SFCCrossCallWatcher) start(sub event.Subscription, chanEvents <-chan *functioncall.SfcCrossCall) {
+func (l *SFCCrossCallRealtimeEventWatcher) start(sub event.Subscription, chanEvents <-chan *functioncall.SfcCrossCall) {
 	logging.Info("Start watching %v...", l.SfcContract)
 	for {
 		select {
 		case err := <-sub.Err():
 			// TODO: communicate this to the calling context
 			logging.Error("error in log subscription %v", err)
-		case log := <-chanEvents:
-			l.Handler.Handle(log)
+		case ev := <-chanEvents:
+			l.Handler.Handle(ev)
 		case <-l.end:
 			logging.Info("Stop watching %v.", l.SfcContract)
 			return
@@ -73,6 +76,106 @@ func (l *SFCCrossCallWatcher) start(sub event.Subscription, chanEvents <-chan *f
 	}
 }
 
-func NewSFCCrossCallWatcher(context context.Context, handler EventHandler, contract *functioncall.Sfc, end chan bool) *SFCCrossCallWatcher {
-	return &SFCCrossCallWatcher{EventWatcherConfig: EventWatcherConfig{Context: context, Handler: handler}, SfcContract: contract, end: end}
+func NewSFCCrossCallRealtimeEventWatcher(context context.Context, handler EventHandler, contract *functioncall.Sfc, end chan bool) *SFCCrossCallRealtimeEventWatcher {
+	return &SFCCrossCallRealtimeEventWatcher{EventWatcherConfig: EventWatcherConfig{Context: context, Handler: handler}, SfcContract: contract, end: end}
+}
+
+type BlockHeadProducer interface {
+	SubscribeNewHead(ctx context.Context, ch chan<- *types.Header) (ethereum.Subscription, error)
+}
+
+// SFCCrossCallFinalisedEventWatcher listens for events from a Simple Function Call bridge and processes these events only once they are
+// finalised. An event is considered finalised once it receives a configurable number of block confirmations.
+// One block confirmation means the instant the transaction generating the event is mined,
+// and is equivalent in behaviour to the SFCCrossCallRealtimeEventWatcher
+type SFCCrossCallFinalisedEventWatcher struct {
+	EventWatcherConfig
+	// confirmationsForFinality refers to the number of block confirmations required before an event is considered finalised.
+	confirmationsForFinality uint64
+	SfcContract              *functioncall.Sfc
+	nextBlockToProcess       uint64
+	client                   BlockHeadProducer
+	end                      chan bool
+}
+
+// Watch subscribes and starts listening to 'CrossCall' events from a given Simple Function Call contract.
+// Once an events receives sufficient block confirmations, it is passed to an event handler for processing.
+func (l *SFCCrossCallFinalisedEventWatcher) Watch() {
+	l.nextBlockToProcess = l.Start
+	headers := make(chan *types.Header)
+
+	sub, err := l.client.SubscribeNewHead(l.Context, headers)
+	if err != nil {
+		log.Fatalf("failed to subscribe to new block headers %v", err)
+	}
+	for {
+		select {
+		case err := <-sub.Err():
+			// TODO: communicate this to the calling context
+			logging.Error("error in log subscription %v", err)
+		case latestHead := <-headers:
+			// TODO: communicate err to the calling context
+			l.processFinalisedEvents(latestHead)
+		}
+	}
+}
+
+func (l *SFCCrossCallFinalisedEventWatcher) processFinalisedEvents(latest *types.Header) error {
+	// TODO: maintain persistent state about last finalised block and observed messages
+	latestBlock := latest.Number.Uint64()
+	confirmations := (latestBlock - l.nextBlockToProcess) + 1
+
+	logging.Debug("latest: '%d', next to process: '%d', confirmations: '%d', required confirmations: %d", latestBlock, l.nextBlockToProcess,
+		confirmations, l.confirmationsForFinality)
+
+	if latestBlock >= l.nextBlockToProcess && confirmations >= l.confirmationsForFinality {
+		numFinalisedBlocks := confirmations - l.confirmationsForFinality
+		startFinalisedBlock := l.nextBlockToProcess
+		lastFinalisedBlock := startFinalisedBlock + numFinalisedBlocks
+
+		logging.Debug("Finalising blocks '%d' to '%d'", startFinalisedBlock, lastFinalisedBlock)
+
+		filterOpts := &bind.FilterOpts{Start: startFinalisedBlock, End: &lastFinalisedBlock, Context: l.Context}
+		finalisedEvs, err := l.SfcContract.FilterCrossCall(filterOpts)
+		if err != nil {
+			logging.Error("error filtering logs from block: %d to %d, error: %v", startFinalisedBlock, lastFinalisedBlock, err)
+			return err
+		}
+
+		// TODO: Handle partial failure scenarios when handling events.
+		// Three cases to consider:
+		// 1. A range of blocks are being processed and some blocks fail
+		// 2. A block is being processed and some events in the block fail processing
+		// 3. Combination of 1 and 2
+		err = l.handleEvents(finalisedEvs)
+		if err != nil {
+			return err
+		}
+
+		l.nextBlockToProcess = lastFinalisedBlock + 1
+	}
+	return nil
+}
+
+func (l *SFCCrossCallFinalisedEventWatcher) handleEvents(events *functioncall.SfcCrossCallIterator) error {
+	for events.Next() {
+		ev := events.Event
+		err := l.Handler.Handle(ev)
+		if err != nil {
+			logging.Error("failed to handle event: %v, error: %v", ev, err)
+			return err
+		}
+	}
+	return nil
+}
+
+// NewSFCCrossCallFinalisedEventWatcher creates an SFCCrossCall event watcher that only returns events once they receive a configured number of
+// confirmations. Note: 1 block confirmation means the instant the transaction generating the event is mined
+func NewSFCCrossCallFinalisedEventWatcher(context context.Context, blockConfirmations uint64, handler EventHandler, contract *functioncall.Sfc,
+	start uint64, client BlockHeadProducer, end chan bool) (*SFCCrossCallFinalisedEventWatcher, error) {
+	if blockConfirmations < 1 {
+		return nil, fmt.Errorf("block confirmationsForFinality cannot be less than 1. supplied value: %d", blockConfirmations)
+	}
+	return &SFCCrossCallFinalisedEventWatcher{EventWatcherConfig: EventWatcherConfig{Context: context, Handler: handler, Start: start},
+		SfcContract: contract, confirmationsForFinality: blockConfirmations, client: client, end: end}, nil
 }
--- messaging/relayer/internal/msgobserver/eth/observer/event_watcher_test.go
+++ messaging/relayer/internal/msgobserver/eth/observer/event_watcher_test.go
@@ -16,11 +16,15 @@ package observer
  */
 
 import (
+	"github.com/consensys/gpact/messaging/relayer/internal/contracts/functioncall"
+	"github.com/consensys/gpact/messaging/relayer/internal/logging"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind/backends"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 	"math/big"
 	"testing"
 	"time"
-
-	"github.com/stretchr/testify/mock"
 )
 
 type MockEventHandler struct {
@@ -32,22 +36,140 @@ func (m *MockEventHandler) Handle(event interface{}) error {
 	return args.Error(0)
 }
 
-func TestSFCCrossCallWatcher(t *testing.T) {
+func TestSFCCrossCallRealtimeEventWatcher(t *testing.T) {
 	simBackend, auth := simulatedBackend(t)
 	contract := deployContract(t, simBackend, auth)
 
 	handler := new(MockEventHandler)
 	handler.On("Handle", mock.AnythingOfType("*functioncall.SfcCrossCall")).Once().Return(nil)
 
-	watcher := NewSFCCrossCallWatcher(auth.Context, handler, contract, make(chan bool))
+	watcher := NewSFCCrossCallRealtimeEventWatcher(auth.Context, handler, contract, make(chan bool))
 	go watcher.Watch()
 
+	makeCrossContractCallTx(t, contract, auth)
+
+	commitAndSleep(simBackend)
+	handler.AssertExpectations(t)
+}
+
+func TestSFCCrossCallFinalisedEventWatcher_FailsIfConfirmationTooLow(t *testing.T) {
+	_, err := NewSFCCrossCallFinalisedEventWatcher(nil, 0, nil, nil, 0,
+		nil, make(chan bool))
+	assert.NotNil(t, err)
+}
+
+// tests the watcher behaviour under different confirmation number settings
+func TestSFCCrossCallFinalisedEventWatcher(t *testing.T) {
+	cases := map[string]struct{ confirmations, start uint64 }{
+		"1 Confirmation":  {1, 2},
+		"2 Confirmations": {2, 1},
+		"6 Confirmations": {6, 1},
+	}
+
+	for k, v := range cases {
+		logging.Info("testing scenario: %s", k)
+		handler := new(MockEventHandler)
+
+		simBackend, auth := simulatedBackend(t)
+		contract := deployContract(t, simBackend, auth)
+
+		watcher, e := NewSFCCrossCallFinalisedEventWatcher(auth.Context, v.confirmations, handler, contract, v.start,
+			simBackend, make(chan bool))
+		assert.Nil(t, e)
+		go watcher.Watch()
+
+		makeCrossContractCallTx(t, contract, auth)
+
+		mineConfirmingBlocks(v.confirmations-1, simBackend)
+		handler.AssertNotCalled(t, "Handle", mock.AnythingOfType("*functioncall.SfcCrossCall"))
+
+		handler.On("Handle", mock.AnythingOfType("*functioncall.SfcCrossCall")).Once().Return(nil)
+		commitAndSleep(simBackend)
+		handler.AssertExpectations(t)
+	}
+}
+
+// tests scenarios where events in multiple blocks have been finalised but not yet been processed
+func TestSFCCrossCallFinalisedEventWatcher_MultipleBlocksFinalised(t *testing.T) {
+	cases := map[string]struct {
+		confirmations, start                uint64
+		ccEventsToCommit, expectedFinalised int
+	}{
+		"Multi-Block-Event-Finalisation-with-1-Confirmation":  {1, 0, 4, 4},
+		"Multi-Block-Event-Finalisation-with-2-Confirmations": {2, 0, 4, 3},
+	}
+
+	for k, v := range cases {
+		logging.Info("testing scenario: %s", k)
+
+		simBackend, auth := simulatedBackend(t)
+		handler := new(MockEventHandler)
+		contract := deployContract(t, simBackend, auth)
+
+		// cross-chain calls before watch instance is started
+		for i := 0; i < v.ccEventsToCommit; i++ {
+			commit(simBackend)
+			makeCrossContractCallTx(t, contract, auth)
+		}
+
+		watcher, e := NewSFCCrossCallFinalisedEventWatcher(auth.Context, v.confirmations, handler, contract, v.start,
+			simBackend, make(chan bool))
+		assert.Nil(t, e)
+		go watcher.Watch()
+
+		handler.On("Handle", mock.AnythingOfType("*functioncall.SfcCrossCall")).Times(v.expectedFinalised).Return(nil)
+		commitAndSleep(simBackend)
+		handler.AssertExpectations(t)
+	}
+}
+
+/*
+Scenario: event is included in a block (b2') that is not part of the canonical chain.
+Given: number of confirmation = 2
+Expectation: the event is not processed at height b3
+	b1 <-- b2 <-- b3 <-- b4
+	  \_ b2'(ev)
+*/
+func TestSFCCrossCallFinalisedEventWatcher_Reorg(t *testing.T) {
+	simBackend, auth := simulatedBackend(t)
+	handler := new(MockEventHandler)
+	contract := deployContract(t, simBackend, auth)
+
+	watcher, e := NewSFCCrossCallFinalisedEventWatcher(auth.Context, 2, handler, contract, 1, simBackend, make(chan bool))
+	assert.Nil(t, e)
+	go watcher.Watch()
+
+	makeCrossContractCallTx(t, contract, auth)
+	b1 := simBackend.Blockchain().CurrentBlock().Hash()
+	commit(simBackend)
+	err := simBackend.Fork(auth.Context, b1)
+	assert.Nil(t, err, "could not simulate forking blockchain")
+
+	mineConfirmingBlocks(2, simBackend)
+	time.Sleep(2 * time.Second)
+
+	// handler would panic if it had been called, so this line is a redundant check for clarity
+	handler.AssertNotCalled(t, "Handle", mock.AnythingOfType("*functioncall.SfcCrossCall"))
+}
+
+func mineConfirmingBlocks(confirms uint64, simBackend *backends.SimulatedBackend) {
+	for i := uint64(0); i < confirms; i++ {
+		commit(simBackend)
+	}
+}
+
+func makeCrossContractCallTx(t *testing.T, contract *functioncall.Sfc, auth *bind.TransactOpts) {
 	_, err := contract.SfcTransactor.CrossBlockchainCall(auth, big.NewInt(100), auth.From, []byte("payload"))
 	if err != nil {
 		failNow(t, "failed to transact: %v", err)
 	}
+}
 
-	simBackend.Commit()
+func commit(backend *backends.SimulatedBackend) {
+	backend.Commit()
+}
+
+func commitAndSleep(backend *backends.SimulatedBackend) {
+	backend.Commit()
 	time.Sleep(2 * time.Second)
-	handler.AssertExpectations(t)
 }
