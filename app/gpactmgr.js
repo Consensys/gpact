ae36dd84d39394e13e8285aa1cc7dea0ff6bc881
wcgcyx <wcgcyx@gmail.com>
2022-03-09 15:54:56 +1000

    Update gpactmgr.js

--- app/gpactmgr.js
+++ app/gpactmgr.js
@@ -11,7 +11,7 @@ export class GPACTManager {
         // Create chain manager.
         var cmgr = new ChainAPManagerMetaMask(ethereum)
         // Create message store manager.
-        var ms = new MsgStore("13.236.2.105:8080")
+        var ms = new MsgStore("3.25.181.96:8080")
         // Create simulator.
         this.simulator = new Simulator(cmgr)
         // Register contract ABI.
