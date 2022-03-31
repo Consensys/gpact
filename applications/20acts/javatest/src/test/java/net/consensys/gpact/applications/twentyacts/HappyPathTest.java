77cc6b6e7f6388b533db5a2fc08b437dafdc75c1
Peter Robinson <drinkcoffee@eml.cc>
2022-03-31 13:20:58 +1000

    Added verify of transacton signature
    Signed-off-by: Peter Robinson <drinkcoffee@eml.cc>

--- applications/20acts/javatest/src/test/java/net/consensys/gpact/applications/twentyacts/HappyPathTest.java
+++ applications/20acts/javatest/src/test/java/net/consensys/gpact/applications/twentyacts/HappyPathTest.java
@@ -26,6 +26,7 @@ import java.util.Random;
 import net.consensys.gpact.applications.twentyacts.crosscontrol.TwentyActsManager;
 import net.consensys.gpact.applications.twentyacts.helpers.TxInfoDigest;
 import net.consensys.gpact.common.BlockchainId;
+import net.consensys.gpact.common.FormatConversion;
 import net.consensys.gpact.common.StatsHolder;
 import net.consensys.gpact.common.Tuple;
 import net.consensys.gpact.messaging.MessagingVerificationInterface;
@@ -33,8 +34,12 @@ import net.consensys.gpact.messaging.SignedEvent;
 import net.consensys.gpact.soliditywrappers.applications.twentyacts.TwentyActs;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.tuweni.bytes.Bytes;
 import org.junit.jupiter.api.Test;
+import org.web3j.crypto.*;
 import org.web3j.protocol.core.methods.response.TransactionReceipt;
+import org.web3j.rlp.*;
+import org.web3j.utils.Numeric;
 
 public class HappyPathTest extends Abstract20ActsTest {
   static final Logger LOG = LogManager.getLogger(HappyPathTest.class);
@@ -98,12 +103,13 @@ public class HappyPathTest extends Abstract20ActsTest {
 
       // user1ChainAErc20.erc20Approve(user1ChainA20Acts.getCbcContractAddress(), total);
       // Create the ERC 20 Approve with the user account (credentials and nonce).
-      String signErc20Approve =
+      String signedErc20Approve =
           this.user1ChainAErc20.createSpecialErc20Approve(
               user1ChainA20Acts.getCbcContractAddress(), total, txInfoDigest);
       // The ERC 20 Approve is communicated to the LP.
       // Submit the signed transaction
-      this.lp1ChainAErc20.sendRawTransaction(signErc20Approve);
+      this.lp1ChainAErc20.sendRawTransaction(signedErc20Approve);
+      verifyTransactionSignature(this.user1, signedErc20Approve, chainA.bcId.asBigInt());
 
       // Prepare on Target
       Tuple<TransactionReceipt, byte[], TwentyActs.PrepareOnTargetEventResponse>
@@ -209,5 +215,114 @@ public class HappyPathTest extends Abstract20ActsTest {
 
     StatsHolder.log("End");
     StatsHolder.print();
+  }
+
+  // The code below analyses how to verify a transaction in code. This is in preparation for
+  // implementing this in Solidity
+  public void verifyTransactionSignature(Credentials credentials, String tx, BigInteger chainId)
+      throws Exception {
+    LOG.info("Tx: {}", tx);
+    Bytes txBytes = Bytes.of(FormatConversion.hexStringToByteArray(tx));
+    LOG.info("Tx: {}", txBytes.toHexString());
+
+    SignedRawTransaction signedRawTx = (SignedRawTransaction) TransactionDecoder.decode(tx);
+    LOG.info("R: {}", Bytes.wrap(signedRawTx.getSignatureData().getR()).toHexString());
+    LOG.info("S: {}", Bytes.wrap(signedRawTx.getSignatureData().getS()).toHexString());
+    LOG.info("V: {}", Bytes.wrap(signedRawTx.getSignatureData().getV()).toHexString());
+    RawTransaction rawTx =
+        RawTransaction.createTransaction(
+            signedRawTx.getNonce(),
+            signedRawTx.getGasPrice(),
+            signedRawTx.getGasLimit(),
+            signedRawTx.getTo(),
+            signedRawTx.getValue(),
+            signedRawTx.getData());
+    byte[] unsignedRawTx = TransactionEncoder.encode(rawTx);
+    Bytes unsignedTxBytes = Bytes.of(unsignedRawTx);
+    LOG.info("Tx[ {}", unsignedTxBytes);
+
+    LOG.info("Len Signed Tx: {}, RawTx: {}", txBytes.size(), unsignedRawTx.length);
+    LOG.info(
+        "Ofs[1]: Signed Tx: {}, RawTx: {}", txBytes.get(1) & 0xff, unsignedTxBytes.get(1) & 0xff);
+
+    final int SIG_LEN = 67;
+    byte v = txBytes.get(txBytes.size() - SIG_LEN);
+    final int DISTANCE_R_LEN_FROM_END = 66;
+    final int DISTANCE_S_LEN_FROM_END = 33;
+    final int RLP_ENCODED_LEN = 0xA0;
+    final int WORD_SIZE = 32;
+
+    int lenOfR = txBytes.get(txBytes.size() - DISTANCE_R_LEN_FROM_END) & 0xff;
+    Bytes r = txBytes.slice(txBytes.size() - DISTANCE_R_LEN_FROM_END + 1, WORD_SIZE);
+    int lenOfS = txBytes.get(txBytes.size() - DISTANCE_R_LEN_FROM_END) & 0xff;
+    Bytes s = txBytes.slice(txBytes.size() - DISTANCE_S_LEN_FROM_END + 1, WORD_SIZE);
+
+    LOG.info("V: {}", v & 0xff);
+    LOG.info("Len of R: {}", lenOfR);
+    LOG.info("Len of S: {}", lenOfS);
+    LOG.info("R: {}", r.toHexString());
+    LOG.info("S: {}", s.toHexString());
+
+    byte[] signTx = FormatConversion.hexStringToByteArray(tx);
+
+    byte transactionType = signTx[0];
+    final int EIP1559_TYPE = 2;
+    if (transactionType == EIP1559_TYPE) {
+      throw new Exception("EIP1559 transactions not supported yet");
+    }
+
+    final RlpList rlpList = RlpDecoder.decode(signTx);
+    final RlpList values = (RlpList) rlpList.getValues().get(0);
+    final BigInteger nonce = ((RlpString) values.getValues().get(0)).asPositiveBigInteger();
+    final BigInteger gasPrice = ((RlpString) values.getValues().get(1)).asPositiveBigInteger();
+    final BigInteger gasLimit = ((RlpString) values.getValues().get(2)).asPositiveBigInteger();
+    final String to = ((RlpString) values.getValues().get(3)).asString();
+    final BigInteger value = ((RlpString) values.getValues().get(4)).asPositiveBigInteger();
+    final String data = ((RlpString) values.getValues().get(5)).asString();
+    final byte[] v1 = ((RlpString) values.getValues().get(6)).getBytes();
+    final byte[] r1 =
+        Numeric.toBytesPadded(
+            Numeric.toBigInt(((RlpString) values.getValues().get(7)).getBytes()), 32);
+    final byte[] s1 =
+        Numeric.toBytesPadded(
+            Numeric.toBigInt(((RlpString) values.getValues().get(8)).getBytes()), 32);
+
+    final int CHAIN_ID_INC = 35;
+    final int LOWER_REAL_V = 27;
+
+    // Remove EIP 155 manipulation of v
+    BigInteger v2 = new BigInteger(1, v1);
+    BigInteger v2a = v2.subtract(BigInteger.valueOf(CHAIN_ID_INC));
+    BigInteger doubleChainId = chainId.multiply(BigInteger.TWO);
+    BigInteger v3 = v2a.subtract(doubleChainId);
+    BigInteger v4 = v3.add(BigInteger.valueOf(LOWER_REAL_V));
+    byte[] v5 = v4.toByteArray();
+    assertEquals(v5.length, 1);
+
+    final Sign.SignatureData signatureData = new Sign.SignatureData(v5, r1, s1);
+
+    List<RlpType> result = new ArrayList<>();
+    result.add(RlpString.create(nonce));
+    result.add(RlpString.create(gasPrice));
+    result.add(RlpString.create(gasLimit));
+    result.add(RlpString.create(Numeric.hexStringToByteArray(to)));
+    result.add(RlpString.create(value));
+
+    // value field will already be hex encoded, so we need to convert into binary first
+    result.add(RlpString.create(FormatConversion.hexStringToByteArray(data)));
+    result.add(RlpString.create(chainId.toByteArray()));
+    result.add(RlpString.create(new byte[] {}));
+    result.add(RlpString.create(new byte[] {}));
+
+    // result.add(RlpString.create(chainId));
+
+    RlpList rlpList1 = new RlpList(result);
+    byte[] tbs = RlpEncoder.encode(rlpList1);
+    LOG.info("Bytes tbs: {}", Bytes.wrap(tbs).toHexString());
+    byte[] messageHash = Hash.sha3(tbs);
+
+    BigInteger pubKey = Sign.signedMessageHashToKey(messageHash, signatureData);
+    LOG.info("Recovered pub key: {}", pubKey.toString(16));
+    LOG.info("Pub Key: {}", credentials.getEcKeyPair().getPublicKey().toString(16));
   }
 }
