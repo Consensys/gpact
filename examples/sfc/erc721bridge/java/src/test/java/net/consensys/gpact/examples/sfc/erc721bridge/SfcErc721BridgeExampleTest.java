09c6dedaae7a4050b116389836dde0c20f964613
Peter Robinson <drinkcoffee@eml.cc>
2022-05-10 12:55:11 +1000

    Temporarily disable SFC tests

--- examples/sfc/erc721bridge/java/src/test/java/net/consensys/gpact/examples/sfc/erc721bridge/SfcErc721BridgeExampleTest.java
+++ examples/sfc/erc721bridge/java/src/test/java/net/consensys/gpact/examples/sfc/erc721bridge/SfcErc721BridgeExampleTest.java
@@ -1,10 +1,13 @@
 package net.consensys.gpact.examples.sfc.erc721bridge;
 
 import net.consensys.gpact.helpers.AbstractExampleTest;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class SfcErc721BridgeExampleTest extends AbstractExampleTest {
 
+  // TODO Remove disable
+  @Disabled
   @Test
   public void directSignMultipleBlockchain() throws Exception {
     String tempPropsFile = createPropertiesFile(true, false, false);
