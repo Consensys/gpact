57f6c6fae4484b6a3615479dd7976f130d21bb6c
Peter Robinson <drinkcoffee@eml.cc>
2022-05-24 17:55:19 +1000

    Add test to make it easy to run the private key generator

---
+++ utils/pkey/src/test/java/net/consensys/gpact/utils/PrivateKeyGeneratorTest.java
@@ -0,0 +1,11 @@
+package net.consensys.gpact.utils;
+
+import org.junit.jupiter.api.Test;
+
+public class PrivateKeyGeneratorTest {
+
+    @Test
+    public void genKey() throws Exception {
+        PrivateKeyGenerator.main(null);
+    }
+}
