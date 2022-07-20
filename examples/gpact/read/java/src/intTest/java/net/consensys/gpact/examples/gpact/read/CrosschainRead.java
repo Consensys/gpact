e81475c5dbc3db88c9794dfc6e5b161039faa512
Peter Robinson <drinkcoffee@eml.cc>
2022-07-20 14:30:13 +1000

    Add parameter to indicate GPACT v1 testing

--- examples/gpact/read/java/src/intTest/java/net/consensys/gpact/examples/gpact/read/CrosschainRead.java
+++ examples/gpact/read/java/src/intTest/java/net/consensys/gpact/examples/gpact/read/CrosschainRead.java
@@ -27,39 +27,45 @@ public class CrosschainRead extends AbstractExampleTest {
   @Test
   public void transferSerialSingleBlockchain() throws Exception {
     String tempPropsFile = createPropertiesFile(false, true, true);
-    Main.main(new String[] {tempPropsFile});
+    Main.main(
+        new String[] {GpactCrossControlManagerGroup.GpactVersion.V1.toString(), tempPropsFile});
   }
 
   @Disabled
   @Test
   public void transferParallelSingleBlockchain() throws Exception {
     String tempPropsFile = createPropertiesFile(false, false, true);
-    Main.main(new String[] {tempPropsFile});
+    Main.main(
+        new String[] {GpactCrossControlManagerGroup.GpactVersion.V1.toString(), tempPropsFile});
   }
 
   @Test
   public void directSignSerialMultiBlockchain() throws Exception {
     String tempPropsFile = createPropertiesFile(true, true, false);
-    Main.main(new String[] {tempPropsFile});
+    Main.main(
+        new String[] {GpactCrossControlManagerGroup.GpactVersion.V1.toString(), tempPropsFile});
   }
 
   @Disabled
   @Test
   public void directSignParallelMultiBlockchain() throws Exception {
     String tempPropsFile = createPropertiesFile(true, false, false);
-    Main.main(new String[] {tempPropsFile});
+    Main.main(
+        new String[] {GpactCrossControlManagerGroup.GpactVersion.V1.toString(), tempPropsFile});
   }
 
   @Test
   public void transferSerialMultiBlockchain() throws Exception {
     String tempPropsFile = createPropertiesFile(false, true, false);
-    Main.main(new String[] {tempPropsFile});
+    Main.main(
+        new String[] {GpactCrossControlManagerGroup.GpactVersion.V1.toString(), tempPropsFile});
   }
 
   @Disabled
   @Test
   public void transferParallelMultiBlockchain() throws Exception {
     String tempPropsFile = createPropertiesFile(false, false, false);
-    Main.main(new String[] {tempPropsFile});
+    Main.main(
+        new String[] {GpactCrossControlManagerGroup.GpactVersion.V1.toString(), tempPropsFile});
   }
 }
